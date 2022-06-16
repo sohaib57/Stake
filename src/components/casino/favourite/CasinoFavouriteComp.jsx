118009a9830892c33f1060d0f9526dfb928fd334
asheikh184 <50655280+asheikh184@users.noreply.github.com>
2022-06-16 15:57:57 +0500

    tt

---
+++ src/components/casino/favourite/CasinoFavouriteComp.jsx
@@ -0,0 +1,115 @@
+import { Heading, Input, InputGroup, InputLeftElement, Stack, Text, chakra, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react'
+import React from 'react'
+import { RiSearch2Line } from 'react-icons/ri'
+import Star from '../../../assets/images/star.png'
+import FavouriteLogo from '../../../assets/images/favouriteLogo.png'
+
+const CasinoFavouriteComp = () => {
+    return (
+        <>
+            {/* Favourite Logo section */}
+            <Stack position={'relative'} minH={'28'} justify={'center'} bgImage={FavouriteLogo} bgRepeat={'no-repeat'} bgPos={'right'} bgColor={'#213743'}>
+                <Heading position={'absolute'} left={'10'} textAlign={'center'} color={'white'}>Favourite</Heading>
+            </Stack>
+            <Stack w={'100%'} bgColor={'#1A2C38'} px={'6'}>
+                <Stack py={'6'}>
+                    {/* Search Section */}
+                    <InputGroup border={'1px solid #2f4553'} borderRadius={'full'}>
+                        <InputLeftElement pointerEvents={'none'} children={<RiSearch2Line color='#4C667B' size={'1.2em'} />} pl={'2'} />
+                        <Input placeholder='Search your game' bgColor={'#0F212E'} borderRadius={'full'} _hover={{ borderColor: '#557086' }} />
+                    </InputGroup>
+                </Stack>
+                <Text color={'#b1bad3'} textAlign={'center'} pb={'6'}>No favourites yet, use the <chakra.span fontWeight={'bold'}>✩</chakra.span> to favourite games.</Text>
+                {/* Tabs Header */}
+                <Stack >
+                    <Tabs variant='unstyled'>
+                        <TabList w={'fit-content'} bgColor={'#0F212E'} borderRadius={'full'}>
+                            <Tab
+                                color={'white'}
+                                m={'1'}
+                                _selected={{
+
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                                _hover={{
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                            > My Bets
+                            </Tab>
+                            <Tab
+                                color={'white'}
+                                m={'1'}
+                                _selected={{
+
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                                _hover={{
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                            > All Bets
+                            </Tab>
+                            <Tab
+                                color={'white'}
+                                m={'1'}
+                                _selected={{
+
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                                _hover={{
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                            > High Rollers
+                            </Tab>
+                            <Tab
+                                color={'white'}
+                                m={'1'}
+                                _selected={{
+
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                                _hover={{
+                                    color: "white",
+                                    bg: "#2F4553",
+                                    borderRadius: 'full',
+                                    m: '1'
+                                }}
+                            > Races 
+                            </Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                                <p>one!</p>
+                            </TabPanel>
+                            <TabPanel>
+                                <p>two!</p>
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </Stack>
+            </Stack>
+        </>
+    )
+}
+
+export default CasinoFavouriteComp
--- src/utils/routes/MainRoutes.jsx
+++ src/utils/routes/MainRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import CasinoFavouriteComp from '../../components/casino/favourite/CasinoFavouriteComp';
 import { CasinoHome } from '../../screens/casino/CasinoHome';
 import Home from '../../screens/home/Home';
 
@@ -7,9 +8,8 @@ const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/casino" element={<CasinoHome/>}>
-
-      </Route>
+      <Route path="/casino" element={<CasinoHome />} />
+      <Route path="/favourite" element={<CasinoFavouriteComp />} />
     </Routes>
   );
 };
