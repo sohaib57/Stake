bff3510c154a79c3f5d684ab365e8e22979cacd0
asheikh184 <50655280+asheikh184@users.noreply.github.com>
2022-06-17 13:10:53 +0500

    tt

---
+++ src/screens/casino/StakeOriginalsScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StakeOriginalsComp from '../../components/casino/Stake Originals/StakeOriginalsComp'
+
+const StakeOriginalsScreen = () => {
+    return (
+        <>
+            <StakeOriginalsComp />
+        </>
+    )
+}
+
+export default StakeOriginalsScreen
--- src/utils/routes/MainRoutes.jsx
+++ src/utils/routes/MainRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { CasinoHome } from '../../screens/casino/CasinoHome';
 import FavouriteScreen from '../../screens/casino/FavouriteScreen';
+import StakeOriginalsScreen from '../../screens/casino/StakeOriginalsScreen';
 import Home from '../../screens/home/Home';
 
 
@@ -11,6 +12,7 @@ const MainRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/casino" element={<CasinoHome />} />
       <Route path="/favourite" element={<FavouriteScreen />} />
+      <Route path="/group/stake-originals" element={<StakeOriginalsScreen />} />
     </Routes>
   );
 };
