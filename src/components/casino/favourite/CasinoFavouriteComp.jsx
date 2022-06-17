977d17e3c4ffb22b0243ec5ff65607e33934d911
asheikh184 <50655280+asheikh184@users.noreply.github.com>
2022-06-17 12:47:52 +0500

    t

--- src/components/casino/favourite/CasinoFavouriteComp.jsx
+++ src/components/casino/favourite/CasinoFavouriteComp.jsx
@@ -81,40 +81,6 @@ const CasinoFavouriteComp = () => {
                         </TabList>
                         <TabPanels>
                             <TabPanel>
-                               
-                                    <Table size='sm'>
-                                        <Thead>
-                                            <Tr>
-                                                <Th>To convert</Th>
-                                                <Th>into</Th>
-                                                <Th isNumeric>multiply by</Th>
-                                            </Tr>
-                                        </Thead>
-                                        <Tbody>
-                                            <Tr>
-                                                <Td>inches</Td>
-                                                <Td>millimetres (mm)</Td>
-                                                <Td isNumeric>25.4</Td>
-                                            </Tr>
-                                            <Tr>
-                                                <Td>feet</Td>
-                                                <Td>centimetres (cm)</Td>
-                                                <Td isNumeric>30.48</Td>
-                                            </Tr>
-                                            <Tr>
-                                                <Td>yards</Td>
-                                                <Td>metres (m)</Td>
-                                                <Td isNumeric>0.91444</Td>
-                                            </Tr>
-                                        </Tbody>
-                                        <Tfoot>
-                                            <Tr>
-                                                <Th>To convert</Th>
-                                                <Th>into</Th>
-                                                <Th isNumeric>multiply by</Th>
-                                            </Tr>
-                                        </Tfoot>
-                                    </Table>
                             </TabPanel>
                         </TabPanels>
                     </Tabs>
