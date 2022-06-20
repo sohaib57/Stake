49a3ec234a6cb15803dd2337b9a17e5a4b2f83e1
sohaib57 <sohaibbutt57@gmail.com>
2022-06-20 11:15:20 +0500

    sohaib

--- src/layout/LayoutComp.jsx
+++ src/layout/LayoutComp.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {  Stack, Spacer } from '@chakra-ui/react';
+import { Stack, Spacer, Img } from '@chakra-ui/react';
 import { Button } from 'antd';
 import { Layout } from 'antd';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
-import { Image } from 'antd';
 import Stake from '../assets/images/stake.png';
 import 'antd/dist/antd.css';
 import './style.css';
@@ -19,7 +18,7 @@ const LayoutComp = ({ children }) => {
       }}
     >
       <Sidebar />
-     
+
       <Layout className="site-layout">
         <Header
           className=".site-header-layout-background"
@@ -29,20 +28,20 @@ const LayoutComp = ({ children }) => {
         >
           <Stack direction={'row'} pr={10}>
             <Stack>
-              <div>
-                <Image src={Stake} alt="stake" width={130} />
-              </div>
+
+              <Img src={Stake} alt="stake" width={130} />
+
             </Stack>
             <Spacer />
-          
-            <div className="button-section">
-            <Button type="text">Sign In</Button>
 
-            <Button type="primary">Register</Button>
-          </div>
-            </Stack>
-          
-           
+            <div className="button-section">
+              <Button type="text">Sign In</Button>
+
+              <Button type="primary">Register</Button>
+            </div>
+          </Stack>
+
+
         </Header>
         <Content className="site-layout-background">
           <Stack
@@ -53,9 +52,9 @@ const LayoutComp = ({ children }) => {
           >
             {children}
             <Footer />
-            
+
           </Stack>
-         
+
         </Content>
       </Layout>
     </Layout>
